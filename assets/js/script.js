8f016f43c0e6e8bd564594aaccd63fb76789ab52
in43sh <in43sh@gmail.com>
2020-01-14 19:49:35 -0800

    Finish js function that changes the color based on clicked option.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,6 @@
 // Add your JS here and include in index.html
 
-// function changeColor(newColor) {
-//     let elem = document.getElementById('cta');
-//     elem.style.color = newColor;
-// }
-
-let cta = document.getElementById('cta');
-let select = document.getElementById('imageColor');
-select.onchange = function () {
-    cta.className = select.options[this.selectedIndex].className;
+function changeColor(color) {
+    let cta = document.getElementById('cta');
+    cta.style.backgroundColor = color;
 }
-
-// var select = document.getElementById('cta');
-// select.onchange = function () {
-//     select.className = this.options[this.selectedIndex].className;
-// }
