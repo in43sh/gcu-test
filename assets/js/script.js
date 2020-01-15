b86c8d3b499e16d36b73253024bb5700e8c19271
in43sh <in43sh@gmail.com>
2020-01-14 19:23:54 -0800

    Start working on js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
 // Add your JS here and include in index.html
+
+// function changeColor(newColor) {
+//     let elem = document.getElementById('cta');
+//     elem.style.color = newColor;
+// }
+
+// let cta = document.getElementById('cta');
+// let select = document.getElementById('imageColor');
+// select.onchange = function () {
+//     cta.className = select.options[this.selectedIndex].className;
+// }
+
+var select = document.getElementById('cta');
+select.onchange = function () {
+    select.className = this.options[this.selectedIndex].className;
+}
