54e3ed19fc6f0f2178da08e85e681c18af9451a4
in43sh <in43sh@gmail.com>
2020-01-14 19:54:39 -0800

    Change js function to arrow function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Add your JS here and include in index.html
 
-function changeColor(color) {
+changeColor = (color) => {
     let cta = document.getElementById('cta');
     cta.style.backgroundColor = color;
 }
